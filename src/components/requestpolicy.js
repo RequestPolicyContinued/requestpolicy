4b22203e541bb8833624a0b079988b36d6b54cf3
Justin Samuel <justin@justinsamuel.com>
2008-12-26 20:46:02 +0000

    Added license block to all js and xul files.

--- src/components/requestpolicy.js
+++ src/components/requestpolicy.js
@@ -1,10 +1,23 @@
-/**
- * RequestPolicy service.
+/*
+ * ***** BEGIN LICENSE BLOCK *****
  * 
- * @author Justin Samuel <justin (at) justinsamuel (dot) com>
- * @copyright 2008
- * @license GPL 3 or Later
- * @link http://www.requestpolicy.com/
+ * RequestPolicy - A Firefox extension for control over cross-site requests.
+ * Copyright (c) 2008 Justin Samuel
+ * 
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * ***** END LICENSE BLOCK *****
  */
 
 const CI = Components.interfaces;

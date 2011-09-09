97103c4c001b3da9c526e90286adb7173b3809b4
Justin Samuel <js@justinsamuel.com>
2011-09-09 12:13:32 -0700

    Fix comment spelling from r461 (#258).

--- src/content/overlay.js
+++ src/content/overlay.js
@@ -1183,7 +1183,7 @@ requestpolicy.overlay = {
         requestpolicy.overlay._stopBlockedContentCheckTimeout();
         requestpolicy.overlay._checkForBlockedContent(content.document);
       },
-      // Though unecessary for Gecko 2.0, I'm leaving in onSecurityChange for
+      // Though unnecessary for Gecko 2.0, I'm leaving in onSecurityChange for
       // SeaMonkey because of https://bugzilla.mozilla.org/show_bug.cgi?id=685466
       onSecurityChange : function() {
       },

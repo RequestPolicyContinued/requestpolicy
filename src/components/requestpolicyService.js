06b7b77f0dc03fe3d0a22a138953189c2c004b54
Justin Samuel <js@justinsamuel.com>
2012-02-04 17:01:35 -0800

    Always allow requests for moz-filedata. (Fixes #277)

--- src/components/requestpolicyService.js
+++ src/components/requestpolicyService.js
@@ -1901,6 +1901,7 @@ RequestPolicyService.prototype = {
         || aContentLocation.scheme == "data"
         || aContentLocation.scheme == "chrome"
         || aContentLocation.scheme == "moz-icon"
+        || aContentLocation.scheme == "moz-filedata"
         || aContentLocation.scheme == "wyciwyg"
         || aContentLocation.scheme == "javascript") {
       return true;

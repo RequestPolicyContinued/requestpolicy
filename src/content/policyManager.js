fd0e940855f07c3a1a702228172f11e46eb2f3a4
Justin Samuel <js@justinsamuel.com>
2012-02-07 12:35:12 -0800

    Make policy manager show policies in a scrolling div using fake data for now.

--- src/content/policyManager.js
+++ src/content/policyManager.js
@@ -1,15 +1,15 @@
 function addPolicyTableRow(table, type, origin, dest) {
-  var rowCount = table.rows.length;
-  var row = table.insertRow(rowCount);
-
-  var typeCell = row.insertCell(0);
-  typeCell.textContent = type;
-
-  var originCell = row.insertCell(1);
-  originCell.textContent = origin;
-
-  var destCell = row.insertCell(2);
-  destCell.textContent = dest;
+//  var rowCount = table.rows.length;
+//  var row = table.insertRow(rowCount);
+//
+//  var typeCell = row.insertCell(0);
+//  typeCell.textContent = type;
+//
+//  var originCell = row.insertCell(1);
+//  originCell.textContent = origin;
+//
+//  var destCell = row.insertCell(2);
+//  destCell.textContent = dest;
 }
 
 // TODO: remove code duplication with menu.js
@@ -49,10 +49,10 @@ function onload() {
 
   var user = policyMgr._policies['user'];
 
-  var el = document.getElementById('policy-user').getElementsByClassName('allow-rule-count')[0];
-  el.textContent = 'There are ' + user.rawPolicy.getAllowRuleCount() + ' "allow" rules in this policy.';
-  var el = document.getElementById('policy-user').getElementsByClassName('deny-rule-count')[0];
-  el.textContent = 'There are ' + user.rawPolicy.getDenyRuleCount() + ' "deny" rules in this policy.';
+//  var el = document.getElementById('policy-user').getElementsByClassName('allow-rule-count')[0];
+//  el.textContent = 'There are ' + user.rawPolicy.getAllowRuleCount() + ' "allow" rules in this policy.';
+//  var el = document.getElementById('policy-user').getElementsByClassName('deny-rule-count')[0];
+//  el.textContent = 'There are ' + user.rawPolicy.getDenyRuleCount() + ' "deny" rules in this policy.';
 
   var table = document.getElementById('policy-user').getElementsByClassName('policy')[0];
 

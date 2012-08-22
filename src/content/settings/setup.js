a25b4d048d39745b60d53fa4fa21336762ec0bdb
Justin Samuel <js@justinsamuel.com>
2012-08-21 21:47:15 -0700

    more on welcome page

--- src/content/settings/setup.js
+++ src/content/settings/setup.js
@@ -1,2 +1,9 @@
+function showConfigure() {
+  document.getElementById('welcome').setAttribute('style', 'display: none');
+  document.getElementById('configure').setAttribute('style', 'display: block');
+}
+
 function onload() {
+  var el = document.getElementById('showconfigure');
+  el.addEventListener('click', showConfigure);
 }

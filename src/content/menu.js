RequestPolicyContinued/requestpolicy

commit 2d0461f2d747d406be95145d41ca3abde4ba02d4

menu options: display number of requests, sorting
Two options have been added.
menu.info.showNumRequests -- default: true
menu.sorting -- default: numRequests

Firstly, support for the number of requests per destination. There is an option to show/hide this information in the menu.

Secondly, there are several options for sorting of the destination-lists.
It's possible to sort by…
(1) …the number of requests (highest number first)
(2) …the destination domain name (alphabetically)
(3) …no sorting at all (like in version 0.5.* until now)

The options can be found in the advanced options.

src/content/menu.js
@@ -183,11 +183,21 @@ requestpolicy.menu = {
   _populateList : function(list, values) {
     this._removeChildren(list);
     if (values[0] && values[0] instanceof requestpolicy.mod.Destination) {
-      values.sort(requestpolicy.mod.Destination.compareFunction);
+      var sorting = this._rpService.prefs.getCharPref('menu.sorting');
+      if (sorting == "numRequests") {
+        values.sort(requestpolicy.mod.Destination.sortByNumRequestsCompareFunction);
+      } else if (sorting == "destName") {
+        values.sort(requestpolicy.mod.Destination.compareFunction);
+      }
       for (var i in values) {
-        var newitem = this._addListItem(list, 'rp-od-item', values[i].dest);
+        var value = values[i];
+        if (true === this._rpService.prefs.getBoolPref('menu.info.showNumRequests')) {
+          var newitem = this._addListItem(list, 'rp-od-item', value.dest, value.properties.numRequests);
+        } else {
+          var newitem = this._addListItem(list, 'rp-od-item', value.dest);
+        }
         newitem.setAttribute("default-policy",
-                            (values[i].properties.numDefaultPolicyRequests > 0 ? "true" : "false"));
+                            (value.properties.numDefaultPolicyRequests > 0 ? "true" : "false"));
       }
     } else {
       values.sort();
@@ -356,13 +366,26 @@ requestpolicy.menu = {
     }
   },
 
-  _addListItem : function(list, cssClass, value) {
-    var item = document.createElement("label");
-    item.setAttribute("value", value);
-    item.setAttribute("class", cssClass);
-    item.setAttribute("onclick", 'requestpolicy.menu.itemSelected(event);');
-    list.insertBefore(item, null);
-    return item;
+  _addListItem : function(list, cssClass, value, numRequests) {
+    var hbox = document.createElement("hbox");
+    hbox.setAttribute("class", cssClass);
+    hbox.setAttribute("onclick", 'requestpolicy.menu.itemSelected(event);');
+    list.insertBefore(hbox, null);
+
+    var destLabel = document.createElement("label");
+    destLabel.setAttribute("value", value);
+    destLabel.setAttribute("class", "destination");
+    destLabel.setAttribute("flex", "2");
+    hbox.insertBefore(destLabel, null);
+
+    if (numRequests) {
+      var numReqLabel = document.createElement("label");
+      numReqLabel.setAttribute("value", numRequests);
+      numReqLabel.setAttribute("class", "numRequests");
+      hbox.insertBefore(numReqLabel, null);
+    }
+
+    return hbox;
   },
 
   _disableIfNoChildren : function(el) {
@@ -411,7 +434,7 @@ requestpolicy.menu = {
   },
 
   _activateDestinationItem : function(item) {
-    this._currentlySelectedDest = item.value;
+    this._currentlySelectedDest = item.getElementsByClassName("destination")[0].value;
 
     if (item.parentNode.id == 'rp-blocked-destinations-list') {
       this._isCurrentlySelectedDestBlocked = true;
@@ -435,6 +458,10 @@ requestpolicy.menu = {
     // TODO: rather than compare IDs, this should probably compare against
     // the elements we already have stored in variables. That is, assuming
     // equality comparisons work that way here.
+    if (item.nodeName == "label" && item.parentNode.nodeName == "hbox") {
+      // item should be the <hbox>
+      item = item.parentNode;
+    }
     if (item.id == 'rp-origin' ||
         item.parentNode.id == 'rp-other-origins-list') {
       this._activateOriginItem(item);

src/content/settings/advancedprefs.html
@@ -90,6 +90,47 @@
           </tr>
         </table>
 
+        <div id="page-subsection" data-string="menuPreferences"></div>
+
+        <table class="settings">
+          <tr>
+            <td data-string="menuSorting"></td>
+            <td>
+              <div>
+                <label><input type="radio" name="menu.sorting" id="sortByNumRequests"
+                              value="numRequests" />
+                  <span data-string="sortByNumRequests"></span>
+                </label><br/>
+                <label><input type="radio" name="menu.sorting" id="sortByDestName"
+                              value="destName" />
+                  <span data-string="sortByDestName"></span>
+                </label><br/>
+                <label><input type="radio" name="menu.sorting" id="noSorting"
+                              value="none" />
+                  <span data-string="noSorting"></span>
+                </label>
+
+                <p>
+                  <span data-string="hint" style="font-weight:bold;"></span> <span data-string="menuSortingHint"></span>
+                </p>
+              </div>
+              <div>
+              </div>
+            </td>
+          </tr>
+          <tr>
+            <td data-string="menuIndicatedInformation"></td>
+            <td>
+              <div>
+                <label>
+                  <input type="checkbox"
+                         id="menu.info.showNumRequests"/>
+                  <span data-string="menuIndicateNumRequests"></span>
+                </label>
+              </div>
+            </td>
+          </tr>
+        </table>
       </div>
     </td>
   </tr>

src/content/settings/advancedprefs.js
@@ -1,5 +1,8 @@
 PAGE_STRINGS = ['basic', 'advanced', 'advancedPreferences', 'linkPrefetching',
-  'dnsPrefetching', 'enabled', 'disableOnStartup', 'restoreDefaultOnUninstall'];
+  'dnsPrefetching', 'enabled', 'disableOnStartup', 'restoreDefaultOnUninstall',
+  'menuPreferences', 'menuSorting', 'sortByNumRequests', 'sortByDestName', 'noSorting',
+  'hint', 'menuSortingHint',
+  'menuIndicatedInformation', 'menuIndicateNumRequests'];
 
 $(function () {
   common.localize(PAGE_STRINGS);
@@ -25,6 +28,25 @@ function updateDisplay() {
 
   document.getElementById('pref-prefetch.dns.restoreDefaultOnUninstall').checked =
       rpService.prefs.getBoolPref('prefetch.dns.restoreDefaultOnUninstall');
+
+  var sorting = rpService.prefs.getCharPref('menu.sorting');
+
+  if (sorting == document.getElementById('sortByNumRequests').value) {
+    document.getElementById('sortByNumRequests').checked = true;
+    document.getElementById('sortByDestName').checked = false;
+    document.getElementById('noSorting').checked = false;
+  } else if (sorting == document.getElementById('noSorting').value) {
+    document.getElementById('sortByNumRequests').checked = false;
+    document.getElementById('sortByDestName').checked = false;
+    document.getElementById('noSorting').checked = true;
+  } else {
+    document.getElementById('sortByNumRequests').checked = false;
+    document.getElementById('sortByDestName').checked = true;
+    document.getElementById('noSorting').checked = false;
+  }
+
+  document.getElementById('menu.info.showNumRequests').checked =
+      rpService.prefs.getBoolPref('menu.info.showNumRequests');
 }
 
 
@@ -71,6 +93,21 @@ function onload() {
   document.getElementById('pref-prefetch.dns.restoreDefaultOnUninstall').addEventListener('change',
       function (event) {
         rpService.prefs.setBoolPref('prefetch.dns.restoreDefaultOnUninstall', event.target.checked);
+        rpService._prefService.savePrefFile(null);
+      }
+  );
+
+  var sortingListener = function (event) {
+    rpService.prefs.setCharPref('menu.sorting', event.target.value);
+    rpService._prefService.savePrefFile(null);
+  };
+  document.getElementById('sortByNumRequests').addEventListener('change', sortingListener);
+  document.getElementById('sortByDestName').addEventListener('change', sortingListener);
+  document.getElementById('noSorting').addEventListener('change', sortingListener);
+
+  document.getElementById('menu.info.showNumRequests').addEventListener('change',
+      function (event) {
+        rpService.prefs.setBoolPref('menu.info.showNumRequests', event.target.checked);
         rpService._prefService.savePrefFile(null);
       }
   );

src/content/settings/settings.css
@@ -100,6 +100,18 @@ text in the side. */
   margin-bottom: 20px;
   border-bottom-width: 1px;
 }
+#page-subsection {
+  font-size: 1.3em;
+  border: 0 solid #eee;
+  font-style: italic;
+
+  padding-top: 10px;
+  margin-top: 30px;
+
+  padding-bottom: 10px;
+  margin-bottom: 20px;
+  border-bottom-width: 1px;
+}
 h2 {
   font-size: 1.3em;
   font-weight: normal;

src/defaults/preferences/defaults.js
@@ -21,6 +21,9 @@ pref("extensions.requestpolicy.prefetch.link.restoreDefaultOnUninstall", true);
 pref("extensions.requestpolicy.prefetch.dns.disableOnStartup", false);
 pref("extensions.requestpolicy.prefetch.dns.restoreDefaultOnUninstall", true);
 
+pref("extensions.requestpolicy.menu.sorting", "numRequests");
+pref("extensions.requestpolicy.menu.info.showNumRequests", true);
+
 pref("extensions.requestpolicy.lastVersion", "0.0");
 pref("extensions.requestpolicy.lastAppVersion", "0.0");
 

src/locale/de/requestpolicy.properties
@@ -48,6 +48,15 @@ dnsPrefetching=DNS Prefetching
 enabled=Aktivieren
 disableOnStartup=Beim Start von Firefox deaktivieren
 restoreDefaultOnUninstall=Bei der Deinstallation von RequestPolicy Standardeinstellung wiederherstellen
+menuPreferences=Menü-Einstellungen
+menuSorting=Sortierung der Ziele
+sortByNumRequests=nach der Anzahl an Anfragen
+sortByDestName=nur nach dem Domainnamen
+noSorting=nicht sortieren
+hint=Hint:
+menuSortingHint=When disabling sorting, the domains are listed in the order they have been requested by the website. This might be helpful in default-deny mode when looking for requests that have to be allowed, as the first requests by the webpage might be essential.
+menuIndicatedInformation=angezeigte Informationen
+menuIndicateNumRequests=die Anzahl an Anfragen für jedes Ziel anzeigen
 yourPolicy=Your Policy
 defaultPolicy=Default Policy
 subscriptions=Subscriptions

src/locale/en-US/requestpolicy.properties
@@ -48,6 +48,15 @@ dnsPrefetching=DNS Prefetching
 enabled=Enabled
 disableOnStartup=Disable on startup
 restoreDefaultOnUninstall=Restore default when RequestPolicy is uninstalled
+menuPreferences=Menu Preferences
+menuSorting=sorting of destinations
+sortByNumRequests=by the number of requests
+sortByDestName=by the domain name only
+noSorting=do not sort
+hint=Hint:
+menuSortingHint=When disabling sorting, the domains are listed in the order they have been requested by the website. This might be helpful in default-deny mode when looking for requests that have to be allowed, as the first requests by the webpage might be essential.
+menuIndicatedInformation=indicated information
+menuIndicateNumRequests=show the number of requests for every destination
 yourPolicy=Your Policy
 defaultPolicy=Default Policy
 subscriptions=Subscriptions

src/locale/eo/requestpolicy.properties
@@ -48,6 +48,15 @@ dnsPrefetching=DNSa antaŭakirado
 enabled=Ŝaltita
 disableOnStartup=Malŝalti ĉe starto de la programo
 restoreDefaultOnUninstall=Restarigi la defaŭltan agordon ĉe malinstalo de RequestPolicy
+menuPreferences=Menu Preferences
+menuSorting=sorting of destinations
+sortByNumRequests=by the number of requests
+sortByDestName=by the domain name only
+noSorting=do not sort
+hint=Hint:
+menuSortingHint=When disabling sorting, the domains are listed in the order they have been requested by the website. This might be helpful in default-deny mode when looking for requests that have to be allowed, as the first requests by the webpage might be essential.
+menuIndicatedInformation=indicated information
+menuIndicateNumRequests=show the number of requests for every destination
 yourPolicy=Your Policy
 defaultPolicy=Default Policy
 subscriptions=Subscriptions

commit 45f833a27fe536607b91c5270c23100ca7d4a9f2

fix a bug introduced with 2d0461f
the bug was related with the "other origins" list. when it was selected,
the destinations didn't populate.

src/content/menu.js
@@ -388,7 +388,7 @@ requestpolicy.menu = {
 
     var destLabel = document.createElement("label");
     destLabel.setAttribute("value", value);
-    destLabel.setAttribute("class", "destination");
+    destLabel.setAttribute("class", "domainname");
     destLabel.setAttribute("flex", "2");
     hbox.insertBefore(destLabel, null);
 
@@ -436,7 +436,13 @@ requestpolicy.menu = {
   },
 
   _activateOriginItem : function(item) {
-    this._currentlySelectedOrigin = item.value;
+    if (item.id == 'rp-origin') {
+      // it's _the_ origin
+      this._currentlySelectedOrigin = item.value;
+    } else if (item.parentNode.id == 'rp-other-origins-list') {
+      // it's an otherOrigin
+      this._currentlySelectedOrigin = item.getElementsByClassName("domainname")[0].value;
+    }
     this._currentlySelectedDest = null;
     // TODO: if the document's origin (rather than an other origin) is being
     // activated, then regenerate the other origins list, as well.
@@ -448,7 +454,7 @@ requestpolicy.menu = {
   },
 
   _activateDestinationItem : function(item) {
-    this._currentlySelectedDest = item.getElementsByClassName("destination")[0].value;
+    this._currentlySelectedDest = item.getElementsByClassName("domainname")[0].value;
 
     if (item.parentNode.id == 'rp-blocked-destinations-list') {
       this._isCurrentlySelectedDestBlocked = true;
b9138b0a18462fb4639256084c2f28029f0add9a
myrdd <myrdd@users.noreply.github.com>
2014-11-06 14:12:28 +0100

    [bugfix] automatically update subscriptions.html on changes (when opened)
    subscriptions.html shows the subscriptions; depending on the default policy, different
    list entries are displayed. So far the page has not been updated when the tab was open
    and the default policy was changed in another tab. See also 576d09d

--- src/content/settings/subscriptions.js
+++ src/content/settings/subscriptions.js
@@ -1,4 +1,4 @@
-PAGE_STRINGS = [
+var PAGE_STRINGS = [
   'yourPolicy',
   'defaultPolicy',
   'subscriptions',
@@ -22,40 +22,93 @@ $(function () {
 
 var prefsChangedObserver = null;
 
+/**
+ * @param {String} policy
+ *                 "allow" or "deny"
+ */
+function getDefaultPolicyElements(policy) {
+  var selector = '[data-default-policy=' + policy + ']';
+  var matches = document.body.querySelectorAll(selector);
+  var elements = Array.prototype.slice.call(matches);
+  return elements;
+}
+
+function displayDefaultPolicyElements(policy, display) {
+  // note: display could be undefined.
+  display = display === false ? false : true;
+  var elements = getDefaultPolicyElements(policy);
+  for (var i = 0, len = elements.length; i < len; i++) {
+    if (display) {
+      elements[i].removeAttribute("style");
+    } else {
+      elements[i].style.display = 'none';
+    }
+  }
+}
+
+function getInputElement(subName) {
+  var elements = document.body.querySelectorAll('input[name=' + subName + ']');
+  if (elements.length <= 0) {
+    return null;
+  }
+  return elements[0];
+}
+
+function getAllSubscriptionElements() {
+  var divs = document.getElementsByClassName("subscription");
+  var elements = [];
+  for (var i = 0, len = divs.length; i < len; ++i) {
+    var div = divs[i];
+    elements.push({
+        id: div.id,
+        div: div,
+        input: getInputElement(div.id)});
+  }
+  return elements;
+}
 
 function updateDisplay() {
   var userSubs = rpService._subscriptions;
   var subsInfo = userSubs.getSubscriptionInfo();
-  for (var subName in subsInfo['official']) {
-    var el = document.getElementById('sub-' + subName);
-    if (!el) {
-      //throw 'Unable to find element with id: sub-' + subName;
-      continue;
-    }
-    el.checked = true;
+  var allSubElements = getAllSubscriptionElements();
+  debugger;
+  for (var i = 0, len = allSubElements.length; i < len; ++i) {
+    var element = allSubElements[i];
+    element.input.checked = (element.id in subsInfo['official']);
+  }
+
+  if (rpService._defaultAllow) {
+    var currentPolicy = 'allow', otherPolicy = 'deny';
+  } else {
+    var currentPolicy = 'deny', otherPolicy = 'allow';
+  }
+  displayDefaultPolicyElements(currentPolicy, true);
+  displayDefaultPolicyElements(otherPolicy, false);
+}
+
+
+
+function handleSubscriptionCheckboxChange(event) {
+  var userSubs = rpService._subscriptions;
+
+  var subName = event.target.name;
+  var enabled = event.target.checked;
+  var subInfo = {};
+  subInfo['official'] = {};
+  subInfo['official'][subName] = true;
+  if (enabled) {
+    userSubs.addSubscription('official', subName);
+    observerService.notifyObservers(null, SUBSCRIPTION_ADDED_TOPIC,
+          JSON.stringify(subInfo));
+  } else {
+    userSubs.removeSubscription('official', subName);
+    observerService.notifyObservers(null, SUBSCRIPTION_REMOVED_TOPIC,
+          JSON.stringify(subInfo));
   }
 }
 
 function onload() {
   updateDisplay();
-  var userSubs = rpService._subscriptions;
-
-  function handleSubscriptionChange(event) {
-    var subName = event.target.name;
-    var enabled = event.target.checked;
-    var subInfo = {};
-    subInfo['official'] = {};
-    subInfo['official'][subName] = true;
-    if (enabled) {
-      userSubs.addSubscription('official', subName);
-      observerService.notifyObservers(null, SUBSCRIPTION_ADDED_TOPIC,
-            JSON.stringify(subInfo));
-    } else {
-      userSubs.removeSubscription('official', subName);
-      observerService.notifyObservers(null, SUBSCRIPTION_REMOVED_TOPIC,
-            JSON.stringify(subInfo));
-    }
-  }
 
   var available = {
     'allow_embedded' : {},
@@ -66,20 +119,12 @@ function onload() {
     'deny_trackers' : {}
   };
   for (var subName in available) {
-    var el = document.getElementById('sub-' + subName);
+    var el = getInputElement(subName);
     if (!el) {
       Logger.dump('Skipping unexpected official subName: ' + subName);
       continue;
     }
-    el.addEventListener('change', handleSubscriptionChange);
-  }
-
-  var selector = '[data-default-policy=' +
-    (rpService._defaultAllow ? 'deny' : 'allow') + ']';
-  var matches = document.body.querySelectorAll(selector);
-  var hideElements = Array.prototype.slice.call(matches);
-  for (var i = 0; i < hideElements.length; i++) {
-    hideElements[i].style.display = 'none';
+    el.addEventListener('change', handleSubscriptionCheckboxChange);
   }
 
   prefsChangedObserver = new common.PrefsChangedObserver(

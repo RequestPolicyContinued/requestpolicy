fb4b5e4a4e67384f013f47f1eb57ce569cc520b4
Martin Kimmerle <dev@256k.de>
2015-08-01 01:50:21 +0200

    xpcshell: fix some URIs

--- tests/xpcshell/test_policymanager.js
+++ tests/xpcshell/test_policymanager.js
@@ -1,4 +1,4 @@
-Components.utils.import("chrome://rpcontinued/content/lib/domain-util.jsm");
+Components.utils.import("chrome://rpcontinued/content/lib/utils/domains.jsm");
 Components.utils.import("chrome://rpcontinued/content/lib/ruleset.jsm");
 Components.utils.import("chrome://rpcontinued/content/lib/policy-manager.jsm");
 

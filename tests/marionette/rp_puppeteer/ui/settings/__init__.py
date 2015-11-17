aac5cff4b60c0851478179c5bc5a9bd0bb130870
Martin Kimmerle <dev@256k.de>
2015-11-17 13:10:45 +0100

    [tst][add] new OldRulesPage library

--- tests/marionette/rp_puppeteer/ui/settings/__init__.py
+++ tests/marionette/rp_puppeteer/ui/settings/__init__.py
@@ -8,6 +8,10 @@ from rp_puppeteer.decorators import use_class_as_property
 
 class Settings(BaseLib):
 
+    @use_class_as_property('ui.settings.old_rules.OldRulesPage')
+    def old_rules(self):
+        pass
+
     @use_class_as_property('ui.settings.your_policy.YourPolicy')
     def your_policy(self):
         pass

c3f81fa8a2f0682626ac968c479bba1927ed1ece
Martin Kimmerle <dev@256k.de>
2015-12-13 17:55:32 +0100

    [tst][ref] link click redirect: convert subfunctions to methods

--- tests/marionette/tests/redirections/test_link_click_redirect.py
+++ tests/marionette/tests/redirections/test_link_click_redirect.py
@@ -10,60 +10,63 @@ PREF_DEFAULT_ALLOW = "extensions.requestpolicy.defaultPolicy.allow"
 
 class TestLinkClickRedirect(RequestPolicyTestCase):
 
-    def setUp(self):
-        super(TestLinkClickRedirect, self).setUp()
-
-        self.prefs.set_pref(PREF_DEFAULT_ALLOW, False)
-
     def tearDown(self):
         try:
             self.prefs.reset_pref(PREF_DEFAULT_ALLOW)
         finally:
             super(TestLinkClickRedirect, self).tearDown()
 
-    def test_redirect_notification_appears_or_not(self):
+    ################
+    # Test Methods #
+    ################
 
-        def test_no_appear(test_url):
-            open_page_and_click_on_first_link(test_url)
+    def test_r21n_appears_or_not__no_rules(self):
+        self.prefs.set_pref(PREF_DEFAULT_ALLOW, False)
 
-            self.assertNotEqual(self.marionette.get_url(), test_url,
-                                "The URL in the urlbar has changed.")
-            self.assertFalse(self.redir.is_shown(),
-                             "There's no redirect notification.")
+        self._test_appear(self._get_url("redirect-js-document-location-link.html",
+                                        generate_page_with_link=False))
 
-        def test_appear(test_url):
-            open_page_and_click_on_first_link(test_url)
+        self._test_appear(self._get_url("redirect-http-location-header.php"))
+        self._test_appear(self._get_url("redirect-http-refresh-header.php"))
+        self._test_appear(self._get_url("redirect-js-document-location-auto.html"))
+        self._test_appear(self._get_url("redirect-meta-tag-01-immediate.html"))
+        self._test_appear(self._get_url("redirect-meta-tag-02-delayed.html"))
+        self._test_appear(self._get_url("redirect-meta-tag-03-multiple.html"))
+        self._test_appear(self._get_url("redirect-meta-tag-08.html"))
 
-            self.assertTrue(self.redir.is_shown(),
-                            "The redirect notification has been displayed.")
+        self._test_no_appear(self._get_url("redirect-meta-tag-04-relative-without-slash.html"))
+        self._test_no_appear(self._get_url("redirect-meta-tag-05-relative-with-slash.html"))
+        self._test_no_appear(self._get_url("redirect-meta-tag-06-different-formatting.html"))
+        self._test_no_appear(self._get_url("redirect-meta-tag-07-different-formatting-delayed.html"))
+        self._test_no_appear(self._get_url("redirect-meta-tag-09-relative.html"))
 
-            self.redir.close()
+    ##########################
+    # Private Helper Methods #
+    ##########################
 
-        def open_page_and_click_on_first_link(test_url):
-            with self.marionette.using_context("content"):
-                self.marionette.navigate(test_url)
-                link = self.marionette.find_element("tag name", "a")
-                link.click()
+    def _test_no_appear(self, test_url):
+        self._open_page_and_click_on_first_link(test_url)
 
-        def get_url(path, generate_page_with_link=True):
-            if generate_page_with_link:
-                path = "link.html?" + path
-            return "http://www.maindomain.test/" + path
+        self.assertNotEqual(self.marionette.get_url(), test_url,
+                            "The URL in the urlbar has changed.")
+        self.assertFalse(self.redir.is_shown(),
+                         "There's no redirect notification.")
 
+    def _test_appear(self, test_url):
+        self._open_page_and_click_on_first_link(test_url)
 
-        test_appear(get_url("redirect-js-document-location-link.html",
-                            generate_page_with_link=False))
+        self.assertTrue(self.redir.is_shown(),
+                        "The redirect notification has been displayed.")
 
-        test_appear(get_url("redirect-http-location-header.php"))
-        test_appear(get_url("redirect-http-refresh-header.php"))
-        test_appear(get_url("redirect-js-document-location-auto.html"))
-        test_appear(get_url("redirect-meta-tag-01-immediate.html"))
-        test_appear(get_url("redirect-meta-tag-02-delayed.html"))
-        test_appear(get_url("redirect-meta-tag-03-multiple.html"))
-        test_appear(get_url("redirect-meta-tag-08.html"))
+        self.redir.close()
 
-        test_no_appear(get_url("redirect-meta-tag-04-relative-without-slash.html"))
-        test_no_appear(get_url("redirect-meta-tag-05-relative-with-slash.html"))
-        test_no_appear(get_url("redirect-meta-tag-06-different-formatting.html"))
-        test_no_appear(get_url("redirect-meta-tag-07-different-formatting-delayed.html"))
-        test_no_appear(get_url("redirect-meta-tag-09-relative.html"))
+    def _open_page_and_click_on_first_link(self, test_url):
+        with self.marionette.using_context("content"):
+            self.marionette.navigate(test_url)
+            link = self.marionette.find_element("tag name", "a")
+            link.click()
+
+    def _get_url(self, path, generate_page_with_link=True):
+        if generate_page_with_link:
+            path = "link.html?" + path
+        return "http://www.maindomain.test/" + path

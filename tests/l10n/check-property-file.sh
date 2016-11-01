2259e0fa3483f9059be947555d88eca2a00652c2
Martin Kimmerle <dev@256k.de>
2016-11-01 16:43:36 +0100

    [test][l10n] create checks for DTD files

--- tests/l10n/check-property-file.sh
+++ tests/l10n/check-property-file.sh
@@ -1,37 +1,16 @@
 #!/bin/bash
 
+source $(dirname "$0")/check-common
+
 main_file="$1"
 ref_file="$2"
-
-fail () {
-  echo -e "Check failed: $1"
-  exit 1
-}
-
-check_file_exists () {
-  local filename="$1"
-  test -f "${filename}" || \
-    fail "File '${filename}' does not exist\!"
-}
 
 # ==============================================================================
 # Checks with a single file
 
 check_file_exists "${main_file}"
-
-check_line_endings () {
-  local n_lines=$(grep -E $'\r'\$ "${main_file}" | wc -l)
-  test "${n_lines}" = "0" || \
-    fail "File '${main_file}' does not have Unix line endings."
-}
-check_line_endings
-
-check_whitespace () {
-  local n_lines=$(grep -E '[[:space:]]$' "${main_file}" | wc -l)
-  test "${n_lines}" = "0" || \
-    fail "File '${main_file}' contains lines with EOL-whitespace."
-}
-check_whitespace
+check_line_endings "${main_file}"
+check_whitespace "${main_file}"
 
 check_placeholders () {
   local lines=$(grep '%S' "${main_file}" | grep -E '%[0-9]+\$S')
@@ -58,13 +37,6 @@ ref__lines=$(get_lines "${ref_file}")
 get_keys () { echo "$1" | grep -Eo '^[^=]+'; }
 main__keys=$(get_keys "${main__lines}")
 ref__keys=$(get_keys "${ref__lines}")
-
-_check_diff () {
-  local diff=$(diff <(echo "${1}") <(echo "${2}"))
-  if [[ "${diff}" != "" ]]; then
-    fail "${3}\nDiff:\n${diff}"
-  fi
-}
 
 check_all_keys_exist () {
   local main__sorted=$(echo "${main__keys}" | sort)

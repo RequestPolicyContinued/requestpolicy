f3e2b691a4264d67b805a2305eee6afc078bc0d1
Martin Kimmerle <dev@256k.de>
2017-10-29 10:26:41 +0100

    [add] model "ui-startup": pWindowsAvailable

---
+++ src/conditional/legacy/content/legacy/lib/utils/xpcom-utils.ts
@@ -0,0 +1,37 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * RequestPolicy - A Firefox extension for control over cross-site requests.
+ * Copyright (c) 2014 Martin Kimmerle
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+
+import {XPCOMObserver, XPCOMObserverTopic} from "content/lib/classes/observer";
+import {defer} from "content/lib/utils/js-utils";
+
+export function promiseObserverTopic(aTopic: XPCOMObserverTopic) {
+  const deferred = defer<[any, string, any]>();
+
+  function callback(subject: any, topic: string, data: any) {
+    observer.unregister();
+    deferred.resolve([subject, topic, data]);
+  }
+
+  const observer = new XPCOMObserver(aTopic, callback);
+
+  return deferred.promise;
+}

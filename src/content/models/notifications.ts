3bf9a9ddbdaadd30200c4a093b88fd632fd5cda4
Martin Kimmerle <dev@256k.de>
2017-10-29 10:27:40 +0100

    [ref] create Notifications model

---
+++ src/content/models/notifications.ts
@@ -0,0 +1,75 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * RequestPolicy - A Firefox extension for control over cross-site requests.
+ * Copyright (c) 2017 Martin Kimmerle
+ *
+ * This program is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+
+import {IEventTarget} from "content/lib/classes/event";
+import {Event} from "content/lib/classes/event";
+import * as WindowUtils from "content/lib/utils/window-utils";
+
+export enum NotificationID {
+  MultipleRPInstallations,
+}
+
+const URI_MAP = new Map([
+  [
+    NotificationID.MultipleRPInstallations,
+    "chrome://rpcontinued/content/multiple-installations.html",
+  ],
+]);
+
+class NotificationsClass extends Set {
+  public onAdded: IEventTarget;
+  public onDeleted: IEventTarget;
+  private events = Event.createMultiple([
+    "onAdded",
+    "onDeleted",
+  ]).events;
+
+  constructor() {
+    super();
+    this.onAdded = this.events.onAdded.eventTarget;
+    this.onDeleted = this.events.onDeleted.eventTarget;
+  }
+
+  public add(aID: NotificationID): this {
+    if (this.has(aID)) return this;
+    super.add(aID);
+    this.events.onAdded.emit(aID);
+    return this;
+  }
+  public delete(aID: NotificationID): boolean {
+    if (!this.has(aID)) return false;
+    this.delete(aID);
+    this.events.onDeleted.emit(aID);
+    return true;
+  }
+
+  public open(aID: NotificationID): boolean {
+    const win = WindowUtils.getMostRecentBrowserWindow();
+    if (win === null) return false;
+    const tabbrowser = win.getBrowser();
+    if (typeof tabbrowser.addTab !== "function") return false;
+    tabbrowser.selectedTab = tabbrowser.addTab(URI_MAP.get(aID));
+    return true;
+  }
+}
+
+export const Notifications = new NotificationsClass();

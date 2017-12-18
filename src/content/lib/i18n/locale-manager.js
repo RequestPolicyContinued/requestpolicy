9e455d59dfca28634ee709a9192bcfd678fba46d
Jérard Devarulrajah <jrrdev@users.noreply.github.com>
2017-12-18 05:58:29 +0100

    Adds the main manager for locales support (i18n)

---
+++ src/content/lib/i18n/locale-manager.js
@@ -0,0 +1,170 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+
+/**
+ * This object is used to manage localization for i18n support.
+ * Original source code is from Mozilla's Gecko Extension.jsm module,
+ * and was modified to suite RequestPolicy needs.
+ * See https://github.com/mozilla/gecko,
+ * gecko/toolkit/components/extensions/Extension.jsm
+ * revision 6991a6334725aabee674e17a2f4f01dc508e4d52.
+ */
+
+ /* global Services */
+
+import {LocaleData} from "content/lib/i18n/locale-data";
+import {ChromeFilesUtils} from "content/lib/utils/chrome-files";
+import {I18nUtils} from "content/lib/i18n/i18n-utils";
+
+
+export const LocaleManager = (function() {
+  let self = {
+    localeData: new LocaleData(),
+    init: null,
+  };
+
+  /**
+   * Return the application locale as a normalized BCP 47 tag.
+   *
+   * @return {String}
+   */
+  self.getAppLocale = function() {
+    let appLocale;
+    if (Services.locale.getAppLocaleAsBCP47) {
+      appLocale = Services.locale.getAppLocaleAsBCP47();
+    } else {
+      // Fallback for older version of gecko
+      let nsILocale = Services.locale.getApplicationLocale();
+      appLocale = nsILocale.getCategory("NSILOCALE_MESSAGES");
+    }
+    return I18nUtils.normalizeToBCP47(appLocale);
+  },
+
+  /**
+   * Return Promise which is fullfilled with a Map(bcp-47-tag -> locale-dir)
+   * of available locales in the _locales dir of the extension.
+   *
+   * @return {Promise}
+   */
+  self.getAvailableLocales = function() {
+    // During build, a JSON file containing all available locales should be
+    // generated.
+    const localesListJSON = ChromeFilesUtils.getChromeUrl(
+      "content/bootstrap/data/locales.json");
+
+    return ChromeFilesUtils.parseJSON(localesListJSON)
+      .then(localesDirNames => {
+        return Promise.all(localesDirNames.map(dirName =>
+          [I18nUtils.normalizeToBCP47(dirName), dirName]));
+      }).then(mapAsArray => new Map(mapAsArray));
+  };
+
+  /**
+   * Return a Map(bcp-47-tag -> locale-dir) which are the best matches
+   * for the requested locales (by the BestAvailableLocale algorithm,
+   * see ecma-402, secton 9.2.2 specification).
+   *
+   * @param {Array} requestedLocales
+   * @param {Map} localesMap a (locale-tag -> locale-dir) map
+   * @return {Map}
+   */
+  self.getBestMatches = function(requestedLocales, localesMap) {
+    let matchesSet = new Set();
+    let localesTag = Array.from(localesMap.keys());
+    for (let locale of requestedLocales) {
+      let normTag = I18nUtils.normalizeToBCP47(locale);
+      let matchTag = I18nUtils.getBestAvailableLocale(localesTag, normTag);
+      if (matchTag) {
+        matchesSet.add(matchTag);
+      }
+    }
+    let result = new Map();
+    for (let key of matchesSet) {
+      result.set(key, localesMap.get(key));
+    }
+    return result;
+  };
+
+  /**
+   * Return a promise which is fullfilled with an array of
+   * {bcp-47-tag, messages} loaded from the map passed in argument.
+   *
+   * @param {Map} localesMap a (bcp-47-tag -> locale-dir) Map
+   * @return {Promise}
+   */
+  self.loadLocalesMessages = function(localesMap) {
+    return Promise.all(Array.from(localesMap.keys()).map(key => {
+      let dir = localesMap.get(key);
+      let file = `content/_locales/${dir}/messages.json`;
+      const url = ChromeFilesUtils.getChromeUrl(file);
+
+      return ChromeFilesUtils.parseJSON(url).then(messages => {
+        return Promise.resolve({"locale": key, "messages": messages});
+      });
+    }));
+  };
+
+  /**
+   * Set the locale to used primarly and its fallback according to
+   * the best matches found in loaded locales.
+   *
+   * @param {String} defaultLocale requested default locale tag
+   * @param {String} uiLocale requested UI locale tag
+   * @return {Object} An object containing the best matches
+   */
+  self.updateLocalesPrefs = function(defaultLocale, uiLocale) {
+    let normDefault = I18nUtils.normalizeToBCP47(defaultLocale);
+    let normUi = I18nUtils.normalizeToBCP47(uiLocale);
+
+    let loadedTags = Array.from(this.localeData.messages.keys()).filter(tag =>
+      tag.toLowerCase() !== this.localeData.BUILTIN.toLowerCase()).map(tag =>
+      I18nUtils.normalizeToBCP47(tag));
+
+    let bestDefault = I18nUtils.getBestAvailableLocale(loadedTags, normDefault);
+    if (!bestDefault) {
+      throw new Error("Can't find a locale matching the default locale "
+        + `'${defaultLocale}'`);
+    }
+
+    let bestUi = I18nUtils.getBestAvailableLocale(loadedTags, normUi);
+    if (!bestUi) {
+      // If a match isn't found, we put the real ui locale code. In fact it
+      // doesn't matter which value we set because defaultLocale will be used
+      bestUi = normUi;
+    }
+    this.localeData.defaultLocale = bestDefault;
+    this.localeData.selectedLocale = bestUi;
+
+    return {
+      selected: bestUi,
+      default: bestDefault,
+    };
+  };
+
+  self.init = function() {
+    const defaultLocale = I18nUtils.normalizeToBCP47("en-US");
+    const uiLocale = this.getAppLocale();
+    const requestedLocales = [defaultLocale, uiLocale];
+
+    return this.getAvailableLocales()
+      .then(AllLocales => this.getBestMatches(requestedLocales, AllLocales))
+      .then(bestMatches => this.loadLocalesMessages(bestMatches))
+      .then(localesMessages => Promise.all(localesMessages.map(
+        obj => this.localeData.addLocale(obj.locale, obj.messages)))
+      ).then(() => this.updateLocalesPrefs(defaultLocale, uiLocale))
+      .catch(e => {
+        console.error("[Fatal] Unable to prepare locales! Details:");
+        console.dir(e);
+        return Promise.reject(e);
+      });
+  };
+
+  return self;
+})();
---
+++ tests/mocha/unit/bootstrap-api/test-locale-manager.js
@@ -0,0 +1,240 @@
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+const sinon = require("sinon");
+
+chai.use(chaiAsPromised);
+const {expect} = chai;
+
+const {LocaleData} = require("content/lib/i18n/locale-data");
+
+// Loads mocking classes
+// Those mocks are needed because some scripts loads XPCOM objects like :
+// const {NetUtil} = Cu.import("resource://gre/modules/NetUtil.jsm");
+const MockNetUtil = require("./lib/mock-netutil");
+const MockComponents = require("./lib/mock-components");
+const MockServices = require("./lib/mock-services");
+
+let sandbox = sinon.createSandbox();
+
+describe("LocaleManager", function() {
+  let ChromeFilesUtils = null;
+  let LocaleManager = null;
+
+  before(function() {
+    let mockComp = new MockComponents();
+    let mockNu = new MockNetUtil();
+    let mockHttp = {httpRequest: function(url, option) {}};
+    let mockServices = new MockServices();
+
+    sinon.stub(mockComp.utils, "import")
+      .withArgs("resource://gre/modules/NetUtil.jsm")
+      .returns({NetUtil: mockNu})
+      .withArgs("resource://gre/modules/Http.jsm")
+      .returns(mockHttp);
+
+    // Replaces global declaration done in bootstrap.js
+    global.Cu = mockComp.utils;
+    global.Services = mockServices;
+
+    ChromeFilesUtils = require("content/lib/utils/chrome-files").ChromeFilesUtils;
+    LocaleManager = require("content/lib/i18n/locale-manager").LocaleManager;
+
+    // Create stubs
+    sinon.stub(ChromeFilesUtils, "parseJSON");
+  });
+
+  afterEach(function() {
+    sandbox.restore();
+    if (global.Service) {
+      global.Service.locale = {};
+    }
+    LocaleManager.localeData = new LocaleData();
+  });
+
+  after(function() {
+    global.Cu = null;
+    global.Services = null;
+  });
+
+  describe("getAppLocale()", function() {
+    it("Should always use getAppLocaleAsBCP47 if available", function() {
+      global.Services.locale = {
+        getAppLocaleAsBCP47: function() {
+          return "fr";
+        },
+        getApplicationLocale: function() {
+          return "en";
+        }
+      };
+
+      let result = LocaleManager.getAppLocale();
+      expect(result).to.equal("fr");
+    });
+
+    it("Should use getApplicationLocale as fallback", function() {
+      global.Services.locale = {
+        getApplicationLocale: function() {
+          return { getCategory: () => "fr" };
+        }
+      };
+
+      let result = LocaleManager.getAppLocale();
+      expect(result).to.equal("fr");
+
+    });
+
+    it("Should return normalized BCP 47 tag", function() {
+      global.Services.locale = {
+        getAppLocaleAsBCP47: function() {
+          return "fr-FR";
+        },
+      };
+
+      let result = LocaleManager.getAppLocale();
+      expect(result).to.equal("fr-fr");
+    });
+
+    it("Should throw RangeError if not a valid BCP 47 tag", function() {
+      global.Services.locale = {
+        getAppLocaleAsBCP47: function() {
+          return "-invalid tag";
+        },
+      };
+
+      let fn = function() { return LocaleManager.getAppLocale(); };
+      expect(fn).to.throws(RangeError);
+    });
+  });
+
+  describe("getAvailableLocales()", function() {
+    it("Should return a map of available locales", function() {
+      ChromeFilesUtils.parseJSON.resolves(["fr", "en"]);
+      let promise = LocaleManager.getAvailableLocales();
+      return expect(promise).to.be.eventually.fulfilled
+        .with.a("map").which.have.all.keys("fr", "en")
+        .and.include("fr")
+        .and.include("en");
+    });
+
+    it("Should normalize tag", function() {
+      ChromeFilesUtils.parseJSON.resolves(["Fr_fR", "eN_Us"]);
+      let promise = LocaleManager.getAvailableLocales();
+      return expect(promise).to.be.eventually.fulfilled
+        .with.a("map").which.have.all.keys("fr-fr", "en-us")
+        .and.include("Fr_fR")
+        .and.include("eN_Us");
+    });
+
+    it("Should be rejected on parseJSON rejection", function() {
+      ChromeFilesUtils.parseJSON.rejects();
+      let promise = LocaleManager.getAvailableLocales();
+      return expect(promise).to.be.rejectedWith(Error);
+    });
+
+    it("Should be rejected on parseJSON exception", function() {
+      ChromeFilesUtils.parseJSON.returns(new Promise((resolve, reject) => {
+        throw new Error();
+      }));
+      let promise = LocaleManager.getAvailableLocales();
+      return expect(promise).to.be.rejectedWith(Error);
+    });
+  });
+
+  describe("getBestMatches(requestedLocales, localesMap)", function() {
+    let localesMap = new Map([["fr", "fr"], ["en-us", "en_US"], ["de", "de"]]);
+
+    it("Should return only requested locales", function() {
+      let result = LocaleManager.getBestMatches(["fr", "en-us"], localesMap);
+      return expect(result).to.be.a("map")
+        .which.have.all.keys("fr", "en-us")
+        .and.include("fr")
+        .and.include("en_US")
+        .and.to.not.have.any.keys("de");
+    });
+
+    it("Should return best match", function() {
+      let result = LocaleManager.getBestMatches(["fr-FR", "en-us"], localesMap);
+      return expect(result).to.be.a("map")
+        .which.have.all.keys("fr", "en-us")
+        .and.include("fr")
+        .and.include("en_US")
+        .and.to.not.have.any.keys("de");
+    });
+
+    it("Shouldn't return duplicates", function() {
+      let result = LocaleManager.getBestMatches(["fr-FR", "fr"], localesMap);
+      return expect(result).to.be.a("map").which.have.property('size', 1);
+    });
+
+    it("Shouldn't empty map if no match", function() {
+      let result = LocaleManager.getBestMatches(["zn"], localesMap);
+      return expect(result).to.be.a("map").which.have.property('size', 0);
+    });
+  });
+
+  describe("loadLocalesMessages(localesMap)", function() {
+    it("Should load all requested locale", function() {
+      let localesMap = new Map([["fr", "fr"], ["en-us", "en_US"]]);
+      ChromeFilesUtils.parseJSON
+        .withArgs("chrome://rpcontinued/content/_locales/fr/messages.json")
+        .resolves("des messages")
+        .withArgs("chrome://rpcontinued/content/_locales/en_US/messages.json")
+        .resolves("some messages");
+      let promise = LocaleManager.loadLocalesMessages(localesMap);
+      return expect(promise).to.be.eventually.fulfilled
+        .with.an("array")
+        .which.deep.include({locale: "fr", messages: "des messages"})
+        .and.deep.include({locale: "en-us", messages: "some messages"});
+    });
+
+    it("Should be rejected if JSON parsing fails", function() {
+      let localesMap = new Map([["fr", "fr"], ["en-us", "en_US"]]);
+      ChromeFilesUtils.parseJSON
+        .withArgs("chrome://rpcontinued/content/_locales/fr/messages.json")
+        .resolves("des messages")
+        .withArgs("chrome://rpcontinued/content/_locales/en_US/messages.json")
+        .rejects();
+
+      let promise = LocaleManager.loadLocalesMessages(localesMap);
+      return expect(promise).to.be.eventually.rejectedWith(Error);
+    });
+  });
+
+  describe("updateLocalesPrefs(defaultLocale, uiLocale)", function() {
+    it("Should find best match for default locale", function() {
+      LocaleManager.localeData.messages = new Map([["en", {}], ["fr", {}]]);
+      let result = LocaleManager.updateLocalesPrefs("en-US", "de");
+      expect(result.default).to.equal("en");
+      expect(LocaleManager.localeData.defaultLocale).to.equal("en");
+    });
+
+    it("Should find best match for ui locale", function() {
+      LocaleManager.localeData.messages = new Map([["en", {}], ["fr", {}]]);
+      let result = LocaleManager.updateLocalesPrefs("en", "fr-FR");
+      expect(result.selected).to.equal("fr");
+      expect(LocaleManager.localeData.selectedLocale).to.equal("fr");
+    });
+
+    it("Should throw error if no match for default locale", function() {
+      LocaleManager.localeData.messages = new Map([["en", {}], ["fr", {}]]);
+      let fn = () => LocaleManager.updateLocalesPrefs("de", "fr");
+      expect(fn).to.throw(Error);
+    });
+
+    it("Should use fallback if no match for ui locale", function() {
+      LocaleManager.localeData.messages = new Map([["en", {}], ["fr", {}]]);
+      let result = LocaleManager.updateLocalesPrefs("en", "de");
+      expect(result.selected).to.equal("de");
+      expect(LocaleManager.localeData.selectedLocale).to.equal("de");
+    });
+
+    it("Should normalized locales tag during search", function() {
+      LocaleManager.localeData.messages = new Map([["en", {}], ["fr", {}]]);
+      let result = LocaleManager.updateLocalesPrefs("En-uS", "fR-fR");
+      expect(result.default).to.equal("en");
+      expect(result.selected).to.equal("fr");
+      expect(LocaleManager.localeData.defaultLocale).to.equal("en");
+      expect(LocaleManager.localeData.selectedLocale).to.equal("fr");
+    });
+  });
+});

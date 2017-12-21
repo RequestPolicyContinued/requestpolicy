0a30e54be4b816fba56aab04d3b957f05058ff06
Jérard Devarulrajah <jrrdev@users.noreply.github.com>
2017-12-21 17:33:21 +0100

    Fixes licensing
     * Fixes l10n.js MIT license header
     * Moves locale-manager.js under GPL. It was initially planned to
    release it under MPL-2.0 because some Firefox code may have been used.
    But as the code was fully rewritten with a different logic,
    MPL-2.0 isn't needed anymore
     * Adds MPL-2.0 header in unit tests files
     * Adds previous informations in LICENSE

--- tests/unit/test-locale-manager.js
+++ tests/unit/test-locale-manager.js
@@ -1,3 +1,13 @@
+/*
+ * ***** BEGIN LICENSE BLOCK *****
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * ***** END LICENSE BLOCK *****
+ */
+ 
 const chai = require("chai");
 const chaiAsPromised = require("chai-as-promised");
 const sinon = require("sinon");

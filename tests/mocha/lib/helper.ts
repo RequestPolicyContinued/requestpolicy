399053cb1c22bcffbff5164553f94f19919614a1
Martin Kimmerle <dev@256k.de>
2018-02-14 13:37:17 +0100

    [test][ref] helper.js -> .ts

---
+++ tests/mocha/lib/helper.ts
@@ -0,0 +1,22 @@
+import { SinonStub } from "sinon";
+
+import {assert} from "chai";
+import * as Sinon from "sinon";
+const sinon = Sinon.sandbox.create();
+
+beforeEach(function() {
+  sinon.stub(console, "error");
+});
+
+afterEach(function() {
+  // FIXME: Something like `this.test.error(err)` would be better, see
+  // https://github.com/mochajs/mocha/pull/1944
+  if ("getCalls" in console.error) {
+    assert.deepEqual(
+        (console.error as SinonStub).getCalls().map((call) => call.args),
+        [],
+        "console.error() shouldn't be called.",
+    );
+  }
+  sinon.restore();
+});

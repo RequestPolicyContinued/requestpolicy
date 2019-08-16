77dbe45fb9513866a19523988f70defe0692455f
Martin Kimmerle <dev@256k.de>
2019-08-16 22:47:44 +0200

    [tests] TestLinkClickRedirectInNewTab: split class into two

--- tests/marionette/tests/redirections/test_link_click_redirect_in_new_tab.py
+++ tests/marionette/tests/redirections/test_link_click_redirect_in_new_tab.py
@@ -12,98 +12,99 @@ from rp_ui_harness.utils import redirections
 PREF_DEFAULT_ALLOW = "extensions.requestpolicy.defaultPolicy.allow"
 
 
-class TestLinkClickRedirectInNewTab(RequestPolicyTestCase):
+class TestLinkClickRedirectInNewTab:
+    class Test(RequestPolicyTestCase):
+        variant_args = None
 
-    def setUp(self):
-        super(TestLinkClickRedirectInNewTab, self).setUp()
+        def setUp(self):
+            super(TestLinkClickRedirectInNewTab.Test, self).setUp()
 
-        self.prefs.set_pref(PREF_DEFAULT_ALLOW, False)
+            self.prefs.set_pref(PREF_DEFAULT_ALLOW, False)
 
-    def tearDown(self):
-        try:
-            self.prefs.reset_pref(PREF_DEFAULT_ALLOW)
-        finally:
-            super(TestLinkClickRedirectInNewTab, self).tearDown()
+        def tearDown(self):
+            try:
+                self.prefs.reset_pref(PREF_DEFAULT_ALLOW)
+            finally:
+                super(TestLinkClickRedirectInNewTab.Test, self).tearDown()
 
-    ################
-    # Test Methods #
-    ################
+        ################
+        # Test Methods #
+        ################
 
-    def test_redirect_notification_appears_or_not(self):
-        tabbar = self.browser.tabbar
+        def test_redirect_notification_appears_or_not(self):
+            tabbar = self.browser.tabbar
 
-        def test_no_appear(test_url, dest_url, info, *args):
-            open_page_and_open_first_link_in_new_tab(test_url, *args)
+            def test_no_appear(test_url, dest_url, info, *args):
+                open_page_and_open_first_link_in_new_tab(test_url, *args)
 
-            # Select the new tab
-            tabbar.tabs[1].select()
+                # Select the new tab
+                tabbar.tabs[1].select()
 
-            redirections.assert_redir_is_shown(
-                self, test_url, dest_url, is_shown=False,
-                additional_info="destination tab")
-            redirections.wait_until_url_load(self, dest_url)
-            redirections.assert_redir_is_shown(
-                self, test_url, dest_url, is_shown=False,
-                additional_info="destination tab")
+                redirections.assert_redir_is_shown(
+                    self, test_url, dest_url, is_shown=False,
+                    additional_info="destination tab")
+                redirections.wait_until_url_load(self, dest_url)
+                redirections.assert_redir_is_shown(
+                    self, test_url, dest_url, is_shown=False,
+                    additional_info="destination tab")
 
-            # Close the new tab.
-            tabbar.close_tab()
+                # Close the new tab.
+                tabbar.close_tab()
 
-            redirections.assert_redir_is_shown(
-                self, test_url, dest_url, is_shown=False,
-                additional_info="origin tab")
+                redirections.assert_redir_is_shown(
+                    self, test_url, dest_url, is_shown=False,
+                    additional_info="origin tab")
 
-        def test_appear(test_url, dest_url, info, *args):
-            open_page_and_open_first_link_in_new_tab(test_url, *args)
+            def test_appear(test_url, dest_url, info, *args):
+                open_page_and_open_first_link_in_new_tab(test_url, *args)
 
-            # Select the new tab
-            tabbar.tabs[1].select()
+                # Select the new tab
+                tabbar.tabs[1].select()
 
-            redirections.assert_redir_is_shown(
-                self, test_url, dest_url, is_shown=True,
-                additional_info="destination tab")
-            redirections.assert_url_does_not_load(
-                self, dest_url,
-                expected_delay=info["delay"])
+                redirections.assert_redir_is_shown(
+                    self, test_url, dest_url, is_shown=True,
+                    additional_info="destination tab")
+                redirections.assert_url_does_not_load(
+                    self, dest_url,
+                    expected_delay=info["delay"])
 
-            # Close the new tab.
-            tabbar.close_tab()
+                # Close the new tab.
+                tabbar.close_tab()
 
-            redirections.assert_redir_is_shown(
-                self, test_url, dest_url, is_shown=False,
-                additional_info="origin tab")
+                redirections.assert_redir_is_shown(
+                    self, test_url, dest_url, is_shown=False,
+                    additional_info="origin tab")
 
-        def open_page_and_open_first_link_in_new_tab(
-            test_url, open_tab_method
-        ):
-            with self.marionette.using_context("content"):
-                self.marionette.navigate(test_url)
-                link = self.marionette.find_element("tag name", "a")
-
-            if open_tab_method == "middleClick":
+            def open_page_and_open_first_link_in_new_tab(
+                test_url, open_tab_method
+            ):
                 with self.marionette.using_context("content"):
-                    Actions(self.marionette).click(link, 1).perform()
-            elif open_tab_method == "contextMenu":
-                self.ctx_menu.select_entry("context-openlinkintab", link)
-                # TODO:
-                #   Use the "tabs" library as soon as it has been ported
-                #   to Marionette, see Mozilla Bug 1121725.
-                #   The mozmill code to open the link in a new tab was:
-                #   ```
-                #   tabBrowser.openTab({method: "contextMenu", target: link});
-                #   ```
+                    self.marionette.navigate(test_url)
+                    link = self.marionette.find_element("tag name", "a")
 
-        def expand_url(path, option="page with link"):
-            if option == "page with link":
-                path = "link.html?" + path
-            return "http://www.maindomain.test/" + path
+                if open_tab_method == "middleClick":
+                    with self.marionette.using_context("content"):
+                        Actions(self.marionette).click(link, 1).perform()
+                elif open_tab_method == "contextMenu":
+                    self.ctx_menu.select_entry("context-openlinkintab", link)
+                    # TODO:
+                    #   Use the "tabs" library as soon as it has been ported
+                    #   to Marionette, see Mozilla Bug 1121725.
+                    #   The mozmill code to open the link in a new tab was:
+                    #   ```
+                    #   tabBrowser.openTab({method: "contextMenu", target: link});
+                    #   ```
 
-        def test_variant(*args):
+            def expand_url(path, option="page with link"):
+                if option == "page with link":
+                    path = "link.html?" + path
+                return "http://www.maindomain.test/" + path
+
             def test(test_url, dest_url, info):
                 if info["is_same_host"]:
-                    test_no_appear(test_url, dest_url, info, *args)
+                    test_no_appear(test_url, dest_url, info, *self.variant_args)
                 else:
-                    test_appear(test_url, dest_url, info, *args)
+                    test_appear(test_url, dest_url, info, *self.variant_args)
 
             def maybe_test((test_url, _, dest_url), info):
                 if (
@@ -145,7 +146,7 @@ class TestLinkClickRedirectInNewTab(RequestPolicyTestCase):
                     # The "Open Link in New Tab" context menu entry is not
                     # available for <a> elements with such hrefs containing
                     # JavaScript code.
-                    if args[0] == "contextMenu":
+                    if self.variant_args[0] == "contextMenu":
                         with self.assertRaises(ElementNotDisplayedException):
                             test(test_url, dest_url, info)
                         return
@@ -156,8 +157,13 @@ class TestLinkClickRedirectInNewTab(RequestPolicyTestCase):
                 redirections.for_each_possible_redirection_scenario(maybe_test,
                                                                     "link")
             except:  # noqa
-                print "test variant: " + str(args[0])
+                print "test variant: " + str(self.variant_args[0])
                 raise
 
-        test_variant("middleClick")
-        test_variant("contextMenu")
+
+class TestLinkClickRedirectInNewTab_MiddleClick(TestLinkClickRedirectInNewTab.Test):
+    variant_args = ["middleClick"]
+
+
+class TestLinkClickRedirectInNewTab_ContextMenu(TestLinkClickRedirectInNewTab.Test):
+    variant_args = ["contextMenu"]
